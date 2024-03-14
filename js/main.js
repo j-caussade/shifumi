d1d9192c11abc326867821d6031a843255ccf56d
Julien Caussade <julien.caussade@gmail.com>
2024-03-14 20:46:32 +0100

    Creation of Automactic vs Computer mode

--- js/main.js
+++ js/main.js
@@ -3,14 +3,18 @@ const startGame = document.querySelector("#start_game");
 const playerComputer = document.querySelector("#player_computer");
 const computerComputer = document.querySelector("#computer_computer");
 
-let playerOrComputer = true;
+let playerVsComputer = true;
 
 const kindPlayer = document.querySelector("#kind_player");
-const panelControl = document.querySelector("#panel_control");
+const panelButtons = document.querySelector("#panel_buttons");
+const controlPanelHeadline = document.querySelector("#control_panel_headline");
+const playButton = document.querySelector("#play");
 
 const displayPlayer = document.querySelector("#left");
 const displayComputer = document.querySelector("#right");
 const moves = document.querySelectorAll(".moves");
+let movePlayer = "";
+let automaticMoveComputer = "";
 
 let displayScorePlayer = document.querySelector("#score_player");
 let displayscoreComputer = document.querySelector("#score_computer");
@@ -21,67 +25,156 @@ const endGame = document.querySelector("#end_game");
 const partyMessage = document.querySelector("#party_message");
 const reStart = document.querySelector("#re_start");
 
-// PARTY:
+// GAME:
 endGame.style.display = "none";
 
 playerComputer.addEventListener("click", playParty);
 computerComputer.addEventListener("click", watchParty);
 
+// PLAYER VS COMPUTER:
 function playParty() {
 	startGame.style.display = "none";
 	kindPlayer.textContent = "Player";
-	panelControl.style.visibility = "visible";
-	playerOrComputer = true;
+	panelButtons.style.display = "flex";
+	playButton.style.display = "none";
+	controlPanelHeadline.textContent = "Choose your moves:";
+
+	// Player:
+	for (let i = 0; i < moves.length; i++) {
+		moves[i].addEventListener("click", selectedMoves);
+	}
+	function selectedMoves(event) {
+		movePlayer = event.target.id;
+
+		displayPlayer.innerHTML = "";
+
+		const move = document.createElement("img");
+		move.setAttribute("id", movePlayer);
+		move.src = `./img/${movePlayer}.png`;
+		displayPlayer.appendChild(move);
+
+		// Computer:
+		const movesComputer = ["papper", "rock", "scissors"];
+		const randomMove = Math.floor(Math.random() * movesComputer.length);
+
+		displayComputer.innerHTML = "";
+
+		const moveComputer = document.createElement("img");
+		moveComputer.setAttribute("id", movesComputer[randomMove]);
+		moveComputer.src = `./img/${movesComputer[randomMove]}.png`;
+		moveComputer.style.transform = "rotateY(180deg)";
+		displayComputer.appendChild(moveComputer);
+
+		// Victory:
+		if (movePlayer == movesComputer[randomMove]) {
+			return;
+		} else if (
+			(movePlayer == "papper") &
+				(movesComputer[randomMove] == "rock") ||
+			(movePlayer == "rock") &
+				(movesComputer[randomMove] == "scissors") ||
+			(movePlayer == "scissors") &
+				(movesComputer[randomMove] == "papper")
+		) {
+			scorePlayer++;
+		} else {
+			scoreComputer++;
+		}
+
+		displayScorePlayer.textContent = scorePlayer;
+		displayscoreComputer.textContent = scoreComputer;
+
+		if (scorePlayer == 10) {
+			endGame.style.display = "flex";
+			partyMessage.textContent = "Victory !";
+		}
+
+		if (scoreComputer == 10) {
+			endGame.style.display = "flex";
+			partyMessage.textContent = "Defeat !";
+		}
+
+		reStart.addEventListener("click", reset);
+
+		// RESET:
+		function reset() {
+			scorePlayer = 0;
+			scoreComputer = 0;
+			displayScorePlayer.textContent = scorePlayer;
+			displayscoreComputer.textContent = scoreComputer;
+			displayPlayer.innerHTML = "";
+			displayComputer.innerHTML = "";
+			endGame.style.display = "none";
+			startGame.style.display = "flex";
+		}
+	}
 }
 
+// AUTOMATIC VS COMPUTER:
 function watchParty() {
 	startGame.style.display = "none";
-	kindPlayer.textContent = "Computer";
-	panelControl.style.visibility = "hidden";
-	playerOrComputer = false;
-}
+	kindPlayer.textContent = "Automatic";
+	panelButtons.style.display = "none";
+	playButton.style.display = "flex";
+	controlPanelHeadline.textContent = "Play a round:";
 
-// PLAYER:
-for (let i = 0; i < moves.length; i++) {
-	moves[i].addEventListener("click", party);
-}
-function party(event) {
-	const movePlayer = event.target.id;
+	// playButton.addEventListener("click", roundPlayer());
+	// function roundPlayer() {}
 
-	displayPlayer.innerHTML = "";
+	while (scorePlayer < 10 && scoreComputer < 10) {
+		// Automatic player
+		function automaticPlayer() {
+			const movesComputer = ["papper", "rock", "scissors"];
+			const randomMove = Math.floor(
+				Math.random() * movesComputer.length
+			);
+			return movesComputer[randomMove];
+		}
+		movePlayer = automaticPlayer();
 
-	const move = document.createElement("img");
-	move.setAttribute("id", movePlayer);
-	move.src = `./img/${movePlayer}.png`;
-	displayPlayer.appendChild(move);
+		displayPlayer.innerHTML = "";
 
-	const movesComputer = ["papper", "rock", "scissors"];
-	const randomMove = Math.floor(Math.random() * movesComputer.length);
+		const move = document.createElement("img");
+		move.setAttribute("id", movePlayer);
+		move.src = `./img/${movePlayer}.png`;
+		displayPlayer.appendChild(move);
 
-	displayComputer.innerHTML = "";
+		// Computer:
+		function automaticComputer() {
+			const movesComputer = ["papper", "rock", "scissors"];
+			const randomMove = Math.floor(
+				Math.random() * movesComputer.length
+			);
+			return movesComputer[randomMove];
+		}
+		automaticMoveComputer = automaticComputer();
 
-	// COMPUTER:
-	const moveComputer = document.createElement("img");
-	moveComputer.setAttribute("id", movesComputer[randomMove]);
-	moveComputer.src = `./img/${movesComputer[randomMove]}.png`;
-	moveComputer.style.transform = "rotateY(180deg)";
-	displayComputer.appendChild(moveComputer);
+		displayComputer.innerHTML = "";
 
-	// VICTORY:
-	if (movePlayer == movesComputer[randomMove]) {
-		return;
-	} else if (
-		(movePlayer == "papper") & (movesComputer[randomMove] == "rock") ||
-		(movePlayer == "rock") & (movesComputer[randomMove] == "scissors") ||
-		(movePlayer == "scissors") & (movesComputer[randomMove] == "papper")
-	) {
-		scorePlayer++;
-	} else {
-		scoreComputer++;
+		const moveComputer = document.createElement("img");
+		moveComputer.setAttribute("id", automaticMoveComputer);
+		moveComputer.src = `./img/${automaticMoveComputer}.png`;
+		moveComputer.style.transform = "rotateY(180deg)";
+		displayComputer.appendChild(moveComputer);
+
+		// Victory:
+		if (movePlayer == automaticComputer()) {
+			scorePlayer += 0;
+			scoreComputer += 0;
+			// return;
+		} else if (
+			(movePlayer == "papper") & (automaticMoveComputer == "rock") ||
+			(movePlayer == "rock") & (automaticMoveComputer == "scissors") ||
+			(movePlayer == "scissors") & (automaticMoveComputer == "papper")
+		) {
+			scorePlayer++;
+		} else {
+			scoreComputer++;
+		}
+
+		displayScorePlayer.textContent = scorePlayer;
+		displayscoreComputer.textContent = scoreComputer;
 	}
-
-	displayScorePlayer.textContent = scorePlayer;
-	displayscoreComputer.textContent = scoreComputer;
 
 	if (scorePlayer == 10) {
 		endGame.style.display = "flex";
@@ -95,7 +188,7 @@ function party(event) {
 
 	reStart.addEventListener("click", reset);
 
-	// RESET:
+	// Reset:
 	function reset() {
 		scorePlayer = 0;
 		scoreComputer = 0;
@@ -107,3 +200,91 @@ function party(event) {
 		startGame.style.display = "flex";
 	}
 }
+
+// function party() {
+// 	// Fonction COMPUTER:
+// 	function playingComputer() {
+// 		const movesComputer = ["papper", "rock", "scissors"];
+// 		const randomMove = Math.floor(Math.random() * movesComputer.length);
+// 		return movesComputer[randomMove];
+// 	}
+
+// 	// PLAYER:
+// 	// condition
+// 	function player() {
+// 		const movePlayer = null;
+// 		if (playerVsComputer == true) {
+// 			for (let i = 0; i < moves.length; i++) {
+// 				moves[i].addEventListener("click", selectedMoves);
+// 			}
+// 			function selectedMoves(event) {
+// 				movePlayer = event.target.id;
+// 			}
+// 		} else {
+// 			movePlayer = playingComputer(movesComputer[randomMove]);
+// 		}
+
+// 		displayPlayer.innerHTML = "";
+
+// 		const move = document.createElement("img");
+// 		move.setAttribute("id", movePlayer);
+// 		move.src = `./img/${movePlayer}.png`;
+// 		displayPlayer.appendChild(move);
+// 	}
+
+// 	// Affichage coup ordinateur
+// 	function displayMoveComputer() {
+// 		displayComputer.innerHTML = "";
+
+// 		const moveComputer = document.createElement("img");
+// 		moveComputer.setAttribute("id", playingComputer());
+// 		moveComputer.src = `./img/${playingComputer()}.png`;
+// 		moveComputer.style.transform = "rotateY(180deg)";
+// 		displayComputer.appendChild(moveComputer);
+// 	}
+
+// 	// VICTORY:
+// 	function victory() {
+// 		if (movePlayer == movesComputer[randomMove]) {
+// 			return;
+// 		} else if (
+// 			(movePlayer == "papper") &
+// 				(movesComputer[randomMove] == "rock") ||
+// 			(movePlayer == "rock") &
+// 				(movesComputer[randomMove] == "scissors") ||
+// 			(movePlayer == "scissors") &
+// 				(movesComputer[randomMove] == "papper")
+// 		) {
+// 			scorePlayer++;
+// 		} else {
+// 			scoreComputer++;
+// 		}
+
+// 		displayScorePlayer.textContent = scorePlayer;
+// 		displayscoreComputer.textContent = scoreComputer;
+
+// 		if (scorePlayer == 10) {
+// 			endGame.style.display = "flex";
+// 			partyMessage.textContent = "Victory !";
+// 		}
+
+// 		if (scoreComputer == 10) {
+// 			endGame.style.display = "flex";
+// 			partyMessage.textContent = "Defeat !";
+// 		}
+
+// 		reStart.addEventListener("click", reset);
+
+// 		// RESET:
+// 		function reset() {
+// 			scorePlayer = 0;
+// 			scoreComputer = 0;
+// 			displayScorePlayer.textContent = scorePlayer;
+// 			displayscoreComputer.textContent = scoreComputer;
+// 			displayPlayer.innerHTML = "";
+// 			displayComputer.innerHTML = "";
+// 			endGame.style.display = "none";
+// 			startGame.style.display = "flex";
+// 		}
+// 	}
+// }
